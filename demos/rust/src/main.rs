f4a3fe2b8b6574c38aec5dd107c7301c98abe0e7
Cesar <cesarbrazon10@gmail.com>
2023-03-27 10:21:44 +0200

    chore: rust demo almost there

--- demos/rust/src/main.rs
+++ demos/rust/src/main.rs
@@ -1,9 +1,8 @@
 use polywrap_core::uri::{ Uri };
-use polywrap_client::{
-    polywrap_client::{ PolywrapClient }
-};
+use polywrap_client::{client::PolywrapClient, msgpack::msgpack};
+
 use polywrap_client_builder::{
-    types::{ BuilderConfig }
+    types::{ BuilderConfig, ClientBuilder, ClientConfigHandler }
 };
 use std::env::{ current_dir };
 use serde::Deserialize;
@@ -13,8 +12,7 @@ struct AddFileResult {
     hash: String
 }
 
-#[tokio::main]
-async fn main() {
+fn main() {
     let ipfs_provider = "http://localhost:5001";
     let uri = Uri::try_from("ens/wraps.eth:ipfs-http-client@1.0.0").unwrap();
     let local_uri = Uri::try_from(format!(
@@ -25,7 +23,7 @@ async fn main() {
     )).unwrap();
 
     let mut builder = BuilderConfig::new(None);
-    builder.add_redirect(uri, local_uri);
+    builder.add_redirect(uri.clone(), local_uri);
     let config = builder.build();
 
     let client = PolywrapClient::new(config);
@@ -37,25 +35,29 @@ async fn main() {
     println!("File Data: {}", file_data);
 
     let add_file_resp = client.invoke::<AddFileResult>(
-        uri,
+        &uri.clone(),
         "addFile",
         Some(&msgpack!({
-            name: file_name,
-            data: file_data.as_bytes(),
-            ipfsProvider: ipfs_provider
-        }))
-    ).await.unwrap();
+            "name": file_name,
+            "data": file_data.as_bytes().to_vec(),
+            "ipfsProvider": ipfs_provider
+        })),
+        None,
+        None
+    ).unwrap();
 
     println!("Successfully Added: {}", add_file_resp.hash);
 
     let cat_resp = client.invoke::<Vec<u8>>(
-        uri,
+        &uri,
         "cat",
         Some(&msgpack!({
-            cid: add_file_resp.hash,
-            ipfsProvider: ipfs_provider
-        }))
-    ).await.unwrap();
+            "cid": add_file_resp.hash,
+            "ipfsProvider": ipfs_provider
+        })),
+        None,
+        None
+    ).unwrap();
 
-    println!("Cat Result: {}", String::from_utf8(cat_resp));
+    println!("Cat Result: {}", String::from_utf8(cat_resp).unwrap());
 }
