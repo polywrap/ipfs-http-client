88aef968953a43b09b2ed5229865ee808920571c
Cesar <cesarbrazon10@gmail.com>
2023-04-27 00:21:11 +0200

    chore: use default config crate & latest version of client crate

--- demos/rust/src/main.rs
+++ demos/rust/src/main.rs
@@ -1,29 +1,22 @@
-use polywrap_core::uri::{ Uri };
-use polywrap_client::{client::PolywrapClient, msgpack::msgpack};
-
-use polywrap_client_builder::{
-    types::{ BuilderConfig, ClientBuilder, ClientConfigHandler }
+use polywrap_client::{
+    builder::types::{BuilderConfig, ClientConfigHandler},
+    client::PolywrapClient,
+    core::uri::Uri,
+    msgpack::msgpack,
 };
-use std::env::{ current_dir };
+use polywrap_client_default_config;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct AddFileResult {
-    hash: String
+    hash: String,
 }
 
 fn main() {
     let ipfs_provider = "http://localhost:5001";
     let uri = Uri::try_from("ens/wraps.eth:ipfs-http-client@1.0.0").unwrap();
-    let local_uri = Uri::try_from(format!(
-        "fs/{}",
-        current_dir().unwrap().as_path().join(
-            "../../wrappers/ipfs-http-client/build"
-        ).display(),
-    )).unwrap();
-
-    let mut builder = BuilderConfig::new(None);
-    builder.add_redirect(uri.clone(), local_uri);
+    let default_config = polywrap_client_default_config::build();
+    let builder = BuilderConfig::new(Some(default_config));
     let config = builder.build();
 
     let client = PolywrapClient::new(config);
@@ -34,30 +27,34 @@ fn main() {
     println!("File Name: {}", file_name);
     println!("File Data: {}", file_data);
 
-    let add_file_resp = client.invoke::<AddFileResult>(
-        &uri.clone(),
-        "addFile",
-        Some(&msgpack!({
-            "name": file_name,
-            "data": file_data.as_bytes().to_vec(),
-            "ipfsProvider": ipfs_provider
-        })),
-        None,
-        None
-    ).unwrap();
+    let add_file_resp = client
+        .invoke::<AddFileResult>(
+            &uri.clone(),
+            "addFile",
+            Some(&msgpack!({
+                "name": file_name,
+                "data": file_data.as_bytes().to_vec(),
+                "ipfsProvider": ipfs_provider
+            })),
+            None,
+            None,
+        )
+        .unwrap();
 
     println!("Successfully Added: {}", add_file_resp.hash);
 
-    let cat_resp = client.invoke::<Vec<u8>>(
-        &uri,
-        "cat",
-        Some(&msgpack!({
-            "cid": add_file_resp.hash,
-            "ipfsProvider": ipfs_provider
-        })),
-        None,
-        None
-    ).unwrap();
+    let cat_resp = client
+        .invoke::<Vec<u8>>(
+            &uri,
+            "cat",
+            Some(&msgpack!({
+                "cid": add_file_resp.hash,
+                "ipfsProvider": ipfs_provider
+            })),
+            None,
+            None,
+        )
+        .unwrap();
 
     println!("Cat Result: {}", String::from_utf8(cat_resp).unwrap());
 }
