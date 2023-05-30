a2a2ca5450f24a9e523080a6aafaeae2af52d7ee
Cesar <cesarbrazon10@gmail.com>
2023-05-30 14:17:12 +0200

    feat: rust demo works as expected

--- demos/rust/src/main.rs
+++ demos/rust/src/main.rs
@@ -2,15 +2,31 @@ use polywrap_client::{
     builder::types::{BuilderConfig, ClientConfigHandler},
     client::PolywrapClient,
     core::uri::Uri,
-    msgpack::msgpack,
+    msgpack::{msgpack, serialize},
 };
 use polywrap_client_default_config;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_bytes::ByteBuf;
 
 #[derive(Deserialize)]
 struct AddFileResult {
     hash: String,
 }
+
+#[derive(Serialize, Deserialize)]
+struct FileEntry {
+    data: Vec<u8>,
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct AddFileArgs {
+    data: FileEntry,
+    ipfsProvider: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct CatResponse;
 
 fn main() {
     let ipfs_provider = "http://localhost:5001";
@@ -27,15 +43,21 @@ fn main() {
     println!("File Name: {}", file_name);
     println!("File Data: {}", file_data);
 
+    let file_entry = FileEntry {
+        data: file_data.as_bytes().to_vec(),
+        name: file_name.to_string(),
+    };
+
+    let add_file_args = AddFileArgs {
+        data: file_entry,
+        ipfsProvider: ipfs_provider.to_string(),
+    };
+
     let add_file_resp = client
         .invoke::<AddFileResult>(
             &uri.clone(),
             "addFile",
-            Some(&msgpack!({
-                "name": file_name,
-                "data": file_data.as_bytes().to_vec(),
-                "ipfsProvider": ipfs_provider
-            })),
+            Some(&serialize(&add_file_args).unwrap()),
             None,
             None,
         )
@@ -44,7 +66,7 @@ fn main() {
     println!("Successfully Added: {}", add_file_resp.hash);
 
     let cat_resp = client
-        .invoke::<Vec<u8>>(
+        .invoke::<ByteBuf>(
             &uri,
             "cat",
             Some(&msgpack!({
@@ -55,6 +77,5 @@ fn main() {
             None,
         )
         .unwrap();
-
-    println!("Cat Result: {}", String::from_utf8(cat_resp).unwrap());
+    println!("Cat Result: {}", String::from_utf8(cat_resp.to_vec()).unwrap());
 }
