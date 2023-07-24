e7e53b5208d5af07c63a6f5a8f8365b8fbc7faea
krisbitney <kris@dorg.tech>
2023-07-24 11:08:34 -0500

    themeing updates for kotlin android ipfs demo

--- demos/kotlin/app/src/main/java/io/polywrap/ipfsdemo/ui/theme/Theme.kt
+++ demos/kotlin/app/src/main/java/io/polywrap/ipfsdemo/ui/theme/Theme.kt
@@ -6,25 +6,20 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color.White,
+    secondary = Color.White,
+    tertiary = Color.Cyan,
+    background = Black,
+    surface = Color.Cyan,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -47,11 +42,11 @@ fun IpfsdemoTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            dynamicDarkColorScheme(context)
         }
 
         darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        else -> DarkColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
